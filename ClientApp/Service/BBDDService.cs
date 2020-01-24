c46ec31c2a2200296570d1a106d918ae7542f6a2
Nonsax <raul.marin@iesdoctorbalmis.com>
2020-01-24 13:59:43 +0100

    Modelo de BBDD terminado e implementacion de los primeros Services

---
+++ ClientApp/Service/BBDDService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClientApp.Model;
+
+namespace ClientApp.Service
+{
+    class BBDDService
+    {
+        private static readonly RestauranteConstanTeamEntities _contexto;
+
+        public static ObservableCollection<categorias> GetCategorias()
+        {
+            return _contexto.categorias.Local;
+        }
+
+        public static ObservableCollection<platos> GetPlatosFromCategory(categorias categoria)
+        {
+            var consulta = from p in _contexto.platos
+                           where p.idCategoria == categoria.idCategoria
+                           orderby p.nombrePlato
+                           select p;
+
+            return new ObservableCollection<platos>(consulta.ToList());
+        }
+
+
+    }
+}
