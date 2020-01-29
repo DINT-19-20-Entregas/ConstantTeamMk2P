d5e80887ae515429a1de1d24aca9f68761000769
MarceloCor98 <ma.cordero@iesdoctorbalmis.com>
2020-01-29 12:30:13 +0100

    InfoPlatoVM primeros cambios

--- ClientApp/ViewModel/InfoPlatoVM.cs
+++ ClientApp/ViewModel/InfoPlatoVM.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using ClientApp.Model;
+using ClientApp.Service;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,26 @@ namespace ClientApp.ViewModel
 {
     class InfoPlatoVM
     {
+        public Plato plato { get; set; }
+        public Pedido pedido { get; set; }
+
+        public ObservableCollection<Ingrediente> listaIngredientes{ get; set; }
+
+
+        public InfoPlatoVM(Plato plato, Pedido pedido)
+        {
+            this.plato = plato;
+            this.pedido = pedido;
+            listaIngredientes = new ObservableCollection<Ingrediente>(plato.ingredientes);
+        }
+
+        public void AñadirPlatoAPedido()
+        {
+            PlatosPorPedido platosPorPedido = new PlatosPorPedido();
+            platosPorPedido.idPedido = pedido.idPedido;
+            platosPorPedido.idPlato = plato.idPlato;
+
+            pedido.platosPorPedido.Add(platosPorPedido);
+        }
     }
 }
